88046199bd91d970d6c1301d5a5656ae6a90fe05
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-04-27 16:32:50 -0700

    initial checkin

---
+++ js/askthensa.js
@@ -0,0 +1,190 @@
+var ascii = {
+    poptart: '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░\n░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░\n░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░\n░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░\n░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░\n░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░\n░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░\n░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░\n░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░\n░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░\n░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░\n░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░\n░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░',
+    nyan: '+      o     +              o   \n    +             o     +       +\no          +\n    o  +           +        +\n+        o     o       +        o\n-_-_-_-_-_-_-_,------,      o\n_-_-_-_-_-_-_-|   /\_/\  \n-_-_-_-_-_-_-~|__( ^ .^)  +     +  \n_-_-_-_-_-_-_-""  ""      \n+      o         o   +       o\n    +         +\no        o         o      o     +\n    o           +\n+      +     o        o      +    ',
+    idl: '                                                                                \n                                                                                \n           :GGLi.                                            .;fGC;             \n           G@@@@@L:                                        :L@@@@@@             \n           L@@@@@@@G;                                    ;C@@@@@@@@             \n           ;@@@@@@@@@G;       ,itLCCGGGGGGGCLfl:.      :C@@@@@@@@@L             \n            @@@@@@@@@@@C, ;LG@@@@@@@@@@@@@@@@@@@@@Cl..C@@@@@@@@@@@:             \n            G@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@              \n            ,@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@C              \n             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,              \n             l@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               \n              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@i               \n              f@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                \n              :@@@@@@@@@G:. .:G@@@@@@@@@@@@@@@@@f,  .l@@@@@@@@@C                \n              @@@@@@@@@C  ,i,  C@@@@@@@@@@@@@@@;  ;;  .@@@@@@@@@;               \n             i@@@@@@@@@  C@@@G  @@@@@@@@@@@@@@L .@@@@i ;@@@@@@@@@               \n             @@@@@@@@@L l@l  il f@@@@@@@@@@@@@, @i .C@. @@@@@@@@@.              \n     .;tfLLLL@@@@@@@@@l CC C@ C l@@@@@@@@@@@@@. L,@t.@; @@@@@@@@@GLLLLti,       \n   ;t;.      @@@@@@@@@f f@.,; L t@@@@@@@@@@@@@. @ i.i@, @@@@@@@@@C      ;ti.    \n :;     ,;ttf@@@@@@@@@@ .GGttG  @@@@@@@@@@@@@@t ;GfL@f ,@@@@@@@@@@tti,     ;;   \n:.    il;.   @@@@@@@@@@t .lfl  t@@@@@@@@@@@@@@@, ,ff;  G@@@@@@@@@L  .:li     :  \n    ,i.      @@@@@@@@@@@f     L@@@@@C,. .,f@@@@@i    ,G@@@@@@@@@@t      ;;      \n   ,.        @@@@@@@@@@@@@GCG@@@@@@@L     t@@@@@@@CC@@@@@@@@@@@@@:        ;     \n             C@@@@@@@@@@@@@@@@@f@@@@@@@@@@@@@@@L@@@@@@@@@@@@@@@@@               \n             ;@@@@@@@@@@@@@@@@@Cf@@@@@@;@@@@@@LL@@@@@@@@@@@@@@@@G               \n              @@@@@@@@@@@@@@@@@@L:tLLi   ;fLt,i@@@@@@@@@@@@@@@@@;               \n              t@@@@@@@@@@@@@@@@@@:iL        C:t@@@@@@@@@@@@@@@@G                \n               @@@@@@@@@@@@@@@@@@l.@       .G C@@@@@@@@@@@@@@@@,                \n               :@@@@@@@@@@@@@@@@@C .        . @@@@@@@@@@@@@@@@l                 \n                ;@@@@@@@@@@@@@@@@@,          l@@@@@@@@@@@@@@@t                  \n                 ;@@@@@@@@@@@@@@@@C          @@@@@@@@@@@@@@@l                   \n                  .G@@@@@@@@@@@@@@@i        L@@@@@@@@@@@@@@,                    \n                    l@@@@@@@@@@@@@@@@     i@@@@@@@@@@@@@@l                      \n                      lG@@@@@@@@@@@@@C:..;G@@@@@@@@@@@Gl                        \n                        :fG@@@@@@@@@@@@@@@@@@@@@@@@Gt,                          \n                           ,;tCG@@@@@@@@@@@@@@GLt:.                             \n                                ..,,:;;;;:,,..                                  \n                                                                                \n                                                                               ',
+    snowden: ';;;;:::,,,,,,,,,...........,:CCGGG@@@@@@@@@@@@@@@G@@L::,,,:,:,:::::::::::::;:i\n;;;:::,,,,,,,,,........,..:fG@@@@@@@@@@@@@@@@@@@@@@@@@@G,,,,,,,:::::::::::::;;\n;;::::,,,,,,,,.........,f@@G@@GGC@@@@@@@@@@@@G@@@@@@@@@@@,,,,,,,,,,::::::,:::;\n;::::,,,,,,,,,.........GGC@fC@@@@@G@@G@@@@@@@@G@@@@@@@@@@@C@;,,,,,:,,::::,:::;\n:::::,,,,,,,,,......l@@@@GGG@@@@CG@@@@@@@@@@@@@@@@@@@@@@@@@G@C,,,,,,,,,:,,:::;\n::::,,,,,,,,,,.....:GfG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@i,,,,,,,,,,:::;\n::::,,,,,,,,......lG@tGGCCGG@@@@@L@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@i:,,,,,,,,,::;\n:::,,,,,,.,,,....i@ffG@fCffGL@@@@@@G@@@@@@@@G@@@@@@@@@@@@@@@@@@@L:,,,,,,,,,,::\n:::,,,,,,.,,....i@@@G@GCGLLG@G@@@CLL@@@GG@@@@@@@@@@@@@@@@@@@@@@@@@l,,,,,,,,,,:\n::,,,,,,,.,....,L@@GffCCffLGCLCLfCG@GGG@G@@@@G@@@G@@@@@@@@@@@@@@@@@f,,,,,,,,,:\n:,,,,,,,,.,,...,G@fLLGGGCLLCttilfttflLGCfftlLtLGCG@@@@@@@@@@@@@@@@@@L:,,,,,,,:\n:,,,,,,,,,,,,.,;GGLLffCCGGLl;:.:,.:,tli,:iii,itltLLC@@@@@@@@@@@@@@@@@C;,,,,,,:\n,,,,,,,,,,,,.,,fLGGL;CLt;,,..,,.,.,; ....,::.:;:;ttfCGC@@@@@@@@@@@@@@@f;,,,,,:\n,,,,,,,,,,.,..,C@@GGCiC:,,........;............:,:itiiLGCGL@@@@@@@@@GCLl:,,,,,\n,,,,,,,,,.....;fGG@@ft;:,......... . . .  ........,,::ilCGG@G@@@@@@@@LLl:..,,:\n,,,,,,,,.......:G@@Gt;:,,......... .   .    .......,,,:lt@@@@@@@@@@@LLf;,..,,,\n,,,,...,.......,L@@ft;:,,.......       ............,,,;tiGG@@@@@@@@G@Lfi,..,,,\n,,,,............;@@fl;:,........    .   .. .........,:itfCLG@@@C@GGLLfi....,,,\n,,,.............,G@fl;,,......              .......,,::;LfLLGCLGGCCLLt;.....,,\n.......... .,...:CLfiLtlfGLi,.. .             ....,,,:;;lCGLCCfLGCLfft,. ...,,\n........   ..,.@GLti;lli;.,iii...  ....tttCfi:.,,.,:::;;iCffCLLtffLLt:.    ..,\n........   ..@@l@@@tttGiG@C@l:;..  ..;::li;;....,:;l:;;;;CCLCCGflfft,.     ,.,\n.... .  ...,,.i.,@@tlttlltt;tL@@@@il:,;lt;@@@il::.@.;lG@@GGC;..i,,ti.     ..,,\n.....   ........ CGliiliiiiit@t,...;::;;i;l@L;LLiil@@Ci;;Lft;  ,:..l.   ....,,\n.......   ...   ;.L;:::::::;lt;...,,..:,;i:::;ii;,: ,,,:;CC;;...; ,Ll.......,,\n........ ....   .i,......,.,i;,...,,,,......,,,,, . ,,,:;f;: ...,.iff:.....,,,\n,....... ....    i:,..,...,:ii,...,,,... ............,:::::....,;:fff,.....,.,\n,............    i:,...,...:l;....::,....  .........,:::::.;,,,::fff:........,\n,..............  ::,,,,....;i.  .:,,;.  .   ........::::;;:....:fff;.........,\n,,,.......... .  ,::,.....:;i.. .,,,;..  .... .....,,;:;l,...,lfffl..........,\n,,,............ .,;:,...:,:;;:;;:.,,..............:,::;itftlfffffi..... .....,\n,,,.............tL:;,;ill;ti;;:,;:,::.............,,;:;Lffffffff,...........,,\n,,,,,.........LG@G;li:;,;:,,,..,:,:;i;,,i.,......,,::;ffff;.     ...........,,\n,,,,,,,......L@GCCli;:...,;li;iilittli:,,;,......,,:;ltffi.    ............,.:\n,,,,,,,....iC@CGG@@l;:,:,................,,..,.,,,,;ttfft,   ..............,,,\n,,,,,,,,,,@@G@@,:lLLili::....,;,..........,.,:,:,:;ftG@GGi. .............,,,,:\n,,,:f@@@@@@G@GLftlCilLl::,..............,:,,.,::;lLl@G@@@@@@,............,,,,C\n@@G@@@G@@@@CCliii;t;:iLLi::,,.,.....,..;ii;i,:;tCil@LLCGGGGLLfGGL,.......,,,,@\n@@@G@@@@@@@ff;;::ll:,,;Cfl:;,,.,,:,,.,:;llittfCiit@@GC@GfCC@CCLGLGCG@;..,,,,,@\nGCCLL@@@@G@tf::::ti:,,.lGfl:;,:,;,:tiittCGGCftiitG@@@GCGL@L@LCCfCGGCLGC@,,,,,L\nCGCLG@@G@@@fC::::l;,,.,..GCCtllllflLf@@@fttfti;lC@@@@GCGCCf@CLCGCGGCLGGGGG:,,L\nGfCfG@GGG@GtG,,,:l:,....,.,,:C@@@@@@CftillltlilLCL@@@GCLGGL@LfLL@C@CCGG@G@GG@G\nLGLGLG@@@@Gt@,,,,::,,......,,,,::::::::;;itfliLCLG@@@@GLf@L@fLLCL@LCG@@@GG@@@ '
+}
+var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var letter_index = 0;
+var MAX_CHARACTERS = 6000;
+
+var trackOptimizely = function(ev) {
+    window['optimizely'] = window['optimizely'] || [];
+    window.optimizely.push(["trackEvent", ev]);
+}
+
+$(document).ready(function(){
+    $('#foia_body').autosize({});
+    $('.fake_dropdown').click(show_menu);
+    $('.blocker').click(hide_menu);
+    $('.menu').on('mouseleave', hide_menu);
+    $('.thumbnail').click(add_ascii);
+    $('.big_red_button').click(submit_form);
+    $('#foia_body').on('keyup', message_change);
+    $('.close').click(hide_modal);
+    message_change();
+
+    var fb = document.querySelectorAll('a.facebook');
+    for (var i = 0; i < fb.length; i++) {
+        fb[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            trackOptimizely('share');
+            window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.askthensa.com%2F');
+        }, false);
+    }
+
+    var tws = document.querySelectorAll('a.twitter');
+    for (var i = 0; i < tws.length; i++) {
+        tws[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            trackOptimizely('share');
+            window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(TWEET_TEXT));
+        }, false);
+    }
+
+    var ems = document.querySelectorAll('a.email');
+    for (var i = 0; i < ems.length; i++) {
+        ems[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            trackOptimizely('share');
+            window.location.href = 'mailto:?subject='+encodeURIComponent(EMAIL_SUBJECT)+'&body='+encodeURIComponent(EMAIL_BODY+'\r\n\r\n')+'https%3A%2F%2Fwww.breakcongressinternet.com%2F';
+        }, false);
+    }
+});
+
+var show_menu = function(e) {
+    if (e.target != $('.fake_dropdown')[0] && e.target != $('.caret')[0])
+        return false;
+
+    $('.menu').slideDown({
+        duration: 200,
+        step: function(now, tween) {
+            if (tween.prop == 'height')
+                tween.start = 44;
+        }
+    });
+    $('.fake_dropdown').addClass('open');
+};
+
+var hide_menu = function() {
+    $('.menu').slideUp({
+        duration: 200,
+        step: function(now, tween) {
+            if (tween.prop == 'height')
+                tween.end = 44;
+        }
+    });
+    $('.fake_dropdown').removeClass('open');
+}
+
+var add_ascii = function(e) {
+    var get_id = function(target) {
+        if (target.className.indexOf('thumbnail') !== -1)
+            return target.id;
+        else
+            return get_id(target.parentNode)
+    }
+    var id = get_id(e.target);
+    
+    $('#foia_body').val($('#foia_body').val() + '\n\nEXHIBIT '+letters[letter_index%26]+':\n\n' + ascii[id] + '\n').trigger('autosize.resize');
+    message_change();
+
+    letter_index++;
+}
+
+var message_change = function(e) {
+    var message = $('#foia_body').val();
+    if (message.length > MAX_CHARACTERS)
+    {
+        message = message.substr(0, MAX_CHARACTERS);
+        $('#foia_body').val(message).trigger('autosize.resize');
+    }
+    $('.num_remaining').html(MAX_CHARACTERS - message.length);
+}
+
+var submit_form = function(e) {
+    e.preventDefault();
+
+    // get the current time at NSA headquarters
+    var d = new Date();
+    d.setUTCHours(d.getUTCHours()-5);   
+    var ts = '{ts \''+d.getUTCFullYear()+'-'+(d.getUTCMonth()+1)+'-'+d.getUTCDate()+' '+(d.getUTCHours() < 10 ? '0'+d.getUTCHours() : d.getUTCHours())+':'+(d.getUTCMinutes() < 10 ? '0'+d.getUTCMinutes() : d.getUTCMinutes())+':'+(d.getUTCSeconds() < 10 ? '0'+d.getUTCSeconds(): d.getUTCSeconds())+'\'}';
+
+    // validate fields
+
+    if (!$('#postal_address').val())
+        return val_err('postal_address', 'Please enter an address.');
+
+    if (!$('#postal_city').val())
+        return val_err('postal_city', 'Please enter a city.');
+
+    if (!$('#postal_state_prov').val())
+        return val_err('postal_state_prov', 'Please enter a state or province.');
+
+    if (!$('#zip_code').val())
+        return val_err('zip_code', 'Please enter a zip code.');
+
+    if (!$('#foia_body').val())
+        return val_err('foia_body', 'Please enter some information that you want the NSA to send you!');
+
+    //var post_url = 'http://www.nsa.gov/applications/forms/foiaemail.cfm';     // JL NOTE ~ doesn't work (XSS protection)
+    //var post_url = '/bury/submit';
+    //var post_url = 'http://debbie:3019/submit';
+    //var post_url = 'http://debbie:9001/foia';
+    var post_url = 'https://queue.fightforthefuture.org/foia';
+
+    $.ajax(post_url, {
+        type: 'POST',
+        data: {
+            'start': ts,
+            'from': '',
+            'Main_Phone': '',
+            'Name_first': $('#name_first').val(),
+            'Name_middle': '',
+            'Name_last': $('#name_last').val(),
+            'email': $('#email').val(),
+            'Company': '',
+            'Postal_Address*': $('#postal_address').val(),
+            'Postal_2nd_Line': '',
+            'Postal_City*': $('#postal_city').val(),
+            'Postal_State-prov*': $('#postal_state_prov').val(),
+            'Zip_Code*': $('#zip_code').val(),
+            'Country': 'United States of America',
+            'Home_Phone': '',
+            'Work_Phone': '',
+            'Work_Extension': '',
+            'Records_Requested*': $('#foia_body').val(),
+            'subscribe': 'true'
+        },
+        complete: function(xhr, status) {
+            console.log('DONE SUBMIT FOIA! Status: ', status);
+            show_modal();
+        }
+    });
+}
+
+var val_err = function(field, message)
+{
+    $('.'+field+'_err').html(message);
+    $('.'+field+'_err').slideDown(200);
+
+    if ($(window).scrollTop() > $('.'+field+'_err').offset().top)
+    {
+        $('html, body').stop().animate({
+            'scrollTop': $('.'+field+'_err').offset().top
+        }, 300, 'swing', function () {
+            // done scrolling lol
+        });
+    }
+}
+
+var show_modal = function()
+{
+    $('#share_modal').css({'display': 'table'});
+    window.setTimeout(function() { $('#share_modal').removeClass('invisible'); }, 25);
+}
+var hide_modal = function()
+{
+    $('#share_modal').addClass('invisible');
+    window.setTimeout(function() {$('#share_modal').hide(); }, 1000);
+}
